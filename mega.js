f9309dc32f38bb31b3a9f9fcb963e924a499b9c7
MidKing01 <139042897+Joshuamambo1@users.noreply.github.com>
2025-08-17 00:00:14 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-  email: 'jadenafrix10@gmail.com', //use your real vaild mega account email
-  password: 'Mahachi2007.', ////use your real vaild mega account password
+  email: 'serrolex@email.com', //use your real vaild mega account email
+  password: 'Mega@1234!', ////use your real vaild mega account password
   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
